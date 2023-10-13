e2104bfad8cddc1d01e1c3d9f7209c4362ad2adb
Pavel Fedotov <p.fedotov@24saas.kz>
2023-10-13 22:51:45 +0600

    Bug fixed for build.sh.

--- build.sh
+++ build.sh
@@ -8,8 +8,7 @@ rm -rf dist/
 
 for key in "${!OS[@]}"
 do
-    echo "$key - ${OS[$key]}"
-    echo "$key - ${ARCH[$key]}"
+    echo Building compressed archive for "${OS[$key]}-${ARCH[$key]}"
     mkdir -p "dist/${OS[$key]}-${ARCH[$key]}"
     cp config.json.example README.md LICENSE "dist/${OS[$key]}-${ARCH[$key]}"
     env GOOS="${OS[$key]}" GOARCH="${ARCH[$key]}" go build -o dist/"${OS[$key]}-${ARCH[$key]}"/cfupdater
